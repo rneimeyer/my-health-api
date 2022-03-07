89d6956ce9ca16bfa7cf4a92ecf0e1e6f63a840b
Kae Saetern <kaesaetern@Kaes-MacBook-Pro.local>
2022-03-07 14:42:11 -0800

    activity model added/updated

--- models/Activity.js
+++ models/Activity.js
@@ -1,1 +1,11 @@
-// activity model
+const mongoose = require("./../db/connection");
+
+const Schema = mongoose.Schema;
+
+const activitySchema = new Schema({
+    "Date": Date,
+    "Type": String,
+    "Length": Number,
+});
+
+module.exports = mongoose.model("Activity", activitySchema)
